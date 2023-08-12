aa937075f9c7c4102ca669b0aeea8e77cfebdc50
paoramati <brandonmitchellpaul@gmail.com>
2023-08-13 10:25:41 +1200

    Multi row span images

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -1,33 +1,148 @@
 import { shuffleArray } from "$lib/utils";
 
-export const images: { imageUrl: string; imageDescription?: string }[] = [
+export type Image = {
+	imageUrl: string;
+	imageDescription?: string;
+	dimensions: {
+		colSpan: 1 | 2 | 3;
+		rowSpan: 1 | 2 | 3;
+	}
+}
+
+export const images: Image[] = [
 
 	{
-		imageUrl: '/images/mosaic_paver_red_blue_white_above.jpeg'
+		imageUrl: '/images/mosaic_paver_red_blue_white_above.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 1
+		}
 	},
 	{
-		imageUrl: '/images/2_mushroom_stools.jpeg'
+		imageUrl: '/images/2_mushroom_stools.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 1
+		}
 	},
 	{
-		imageUrl: '/images/3_mosaic_stones.jpeg'
+		imageUrl: '/images/3_mosaic_stones.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 1
+		}
 	},
 	{
-		imageUrl: '/images/flower_mandala_paver_above.jpeg'
+		imageUrl: '/images/flower_mandala_paver_above.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 1
+		}
+	},
+	// {
+	// 	imageUrl: '/images/mosaic_paver_progress_4.jpeg',
+	// 	dimensions: {
+	// 		colSpan: 1,
+	// 		rowSpan: 1
+	// 	}
+	// },
+	{
+		imageUrl: '/images/mosaic_paver_purple_blue_white_above.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 1
+		}
 	},
 	{
-		imageUrl: '/images/mosaic_paver_progress_4.jpeg'
+		imageUrl: '/images/mosaic_stone_and_lettuce.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 1
+		}
 	},
 	{
-		imageUrl: '/images/mosaic_paver_purple_blue_white_above.jpeg'
+		imageUrl: '/images/mosaic_stone_whit_blue.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 1
+		}
 	},
 	{
-		imageUrl: '/images/mosaic_stone_and_lettuce.jpeg'
+		imageUrl: '/images/pastel_mandala_paver_above.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 1
+		}
 	},
 	{
-		imageUrl: '/images/mosaic_stone_whit_blue.jpeg'
+		imageUrl: '/images/2_mosaic_paver.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 1
+		}
 	},
 	{
-		imageUrl: '/images/pastel_mandala_paver_above.jpeg'
+		imageUrl: '/images/mosaic_paver_rainbow_above.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 1
+		}
+	},
+	{
+		imageUrl: '/images/mosaic_paver_red_welcome_above.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 1
+		}
+	},
+	{
+		imageUrl: '/images/purple_bowl_inverted.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 1
+		}
+	},
+	{
+		imageUrl: '/images/mosaic_paver_trail.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 2
+		}
+	},
+	{
+		imageUrl: '/images/mosaic_paver_trail_2.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 2
+		}
+	},
+	{
+		imageUrl: '/images/mosaic_stone_and_pawa.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 2
+		}
+	},
+	{
+		imageUrl: '/images/mosaic_paver_trail_3.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 2
+		}
+	},
+	{
+		imageUrl: '/images/mushroom_stool_flower.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 2
+		}
+	},
+	{
+		imageUrl: '/images/white_purple_mosaic_paver__above.jpeg',
+		dimensions: {
+			colSpan: 1,
+			rowSpan: 1
+		}
 	},
 ];
 
@@ -35,5 +150,5 @@ export function getImagesRandomOrder() {
 
 	const images2 = [...images];
 
-	return shuffleArray(images2);
+	return shuffleArray(images2) as Image[];
 }
