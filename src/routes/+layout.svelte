ee1db0b988732eeeb0fb6dc5652a4f5108373160
Brandon Paul <brandonmitchellpaul@gmail.com>
2023-08-13 12:29:25 +1200

    cleanups

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,10 @@
 <script lang="ts">
-	// The ordering of these imports is critical to your app working properly
-	import '@skeletonlabs/skeleton/themes/theme-gold-nouveau.css';
-	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
-	import '@skeletonlabs/skeleton/styles/skeleton.css';
-	// Most of your app wide CSS should be put in this file
-	import '../app.postcss';
+	import './style-imports';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 </script>
 
-<!-- App Shell -->
 <AppShell>
 	<svelte:fragment slot="header">
-		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
 				<strong class="text-xl uppercase">Ness Garden Art</strong>
@@ -33,14 +26,14 @@
 				>
 					Gallery
 				</a>
-				<!-- <a
+				<!-- <ag
 					class="btn btn-sm variant-ghost-surface"
 					href="https://github.com/skeletonlabs/skeleton"
 					target="_blank"
 					rel="noreferrer"
 				>
 					GitHub
-				</a> -->
+				</ag> -->
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
